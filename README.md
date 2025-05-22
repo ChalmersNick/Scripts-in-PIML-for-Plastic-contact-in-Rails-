c85b70afc5f2d7717b37e9edeff996b85d2fb836
Marcus Petersson <marcpete@chalmers.se>
2025-05-22 09:49:38 +0200

    uploaded excel sheets with data

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Welcome to this repository!
-
-It contains scripts and datafiles connected to a project in a course in the MSc in Applied mechanics program at Chalmers University of Technology. The project regarded simulating training data with Abaqus to train an ML model that can predict different quantities regarding contact between a train wheel and a rail. The purpose was to make a faster calculation with the model instead of relying on heavy simulations for accurate results.
+It contains scripts and datafiles connected to a project in the course TME131 Project in Applied Mechanics in the MSc in Applied Mechanics program at Chalmers University of Technology. The project regarded simulating training data with Abaqus to train an ML model that can predict different quantities (max von Mises stress, max contact pressure, etc.) regarding contact between a train wheel and a rail. The purpose was to make a faster calculation with the model instead of relying on heavy simulations for accurate results.
 
 ## Info about the code
 The simulations were automated with Abaqus Python interpreter and the code for the simulation and reading of the data can be found here, along with the scripts that trained the ML model.
--- Simulation/odb_to_json.py
+++ Simulation/odb_to_json.py
@@ -1,47 +1,26 @@
 from odbAccess import openOdb
 import json
-from pathlib import Path
-
 
 # Fetch and open the ODB and the step
-
-
 generated_path = "MainJob.odb"
-odb = openOdb(path=generated_path)
+odb = openOdb(path = generated_path)
 
-#odb = openOdb(path=r'MainOutput\\MainJob.odb')
 step_names = list(odb.steps.keys())
 frame = odb.steps[step_names[-1]].frames[-1]
 
 # CPRESS field
 cpress_field = frame.fieldOutputs['CPRESS']
-
-cpress_list = []
-for value in cpress_field.values:
-    cpress_list.append(value.data)
-
+cpress_list = [value.data for value in cpress_field.values]
 max_cpress = max(cpress_list)
 
 # Stress field
 stress = frame.fieldOutputs['S']
-
-stress_list = []
-for value in stress.values:
-    vm = value.mises  # This is the von Mises stress
-    stress_list.append(vm)
-
+stress_list = [value.mises for value in stress.values]
 max_vonmises = max(stress_list)
 
 # Displacement field
 u_field = frame.fieldOutputs['U']
-
-u_list = []
-for value in u_field.values:
-    u2 = value.data[1]  # U2 is the second component (index 1)
-    u_list.append(u2)
-    
-    #print(f"Node: {value.nodeLabel}, U2: {u2}") kan få ut vilken nod oxå om man vill
-
+u_list = [value.data[1] for value in u_field.values]    
 min_u = min(u_list)
 
 # Dump into json file
